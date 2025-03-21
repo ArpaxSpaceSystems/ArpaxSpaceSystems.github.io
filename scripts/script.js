e9b7a92a4a7415af354bac8aa6f3cfbef0feb8a5
ArpaxSpaceSystems <arpaxspace@gmail.com>
2025-03-21 11:40:22 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
     changeLanguage(savedLanguage);
 
     //changeLanguage('en');
-    document.getElementById('current-year').textContent = new Date().getFullYear();
+    
 });
 
-
+document.getElementById('current-year').textContent = new Date().getFullYear();
